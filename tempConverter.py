04c8ee80f22cd60c984405da54ed4f39ec3bac08
Hyacinth21 <japzonmark94@gmail.com>
2025-07-16 13:34:16 +0800

    Update tempConverter.py

--- tempConverter.py
+++ tempConverter.py
@@ -1,12 +1,12 @@
 num1 = float(input("Temperature Value: "))
-unit = input("Temperature Value (C or F): ")
+unit = (input("Temperature Unit (C or F): "))
 
 #Process
 if unit.upper() == "C": 
-    result = (num1 * 9/5) + 32
+    result = (num1 * 5/9) - 32
+    print(f"Result: {result}°F")
 elif unit.upper () == "F":
-    result = (num1 - 32) * 9/5
+    result = (num1 * 9/5) + 32
+    print(f"Result: {result}°C")
 else:
     result = "Invalid output"
-
-print(f"Result: {result}")
